9574f9c54ca137e291d2eb868c07fa3c001169b3
Dipen <dipendraboyaju222@gmail.com>
2025-02-16 23:49:48 +0545

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,13 @@ import vid2 from '../assets/videos/vid2.mp4'
 
 const Hero = () => {
   return (
-    <div className="container mx-auto py-32 px-10">
+    <div className="container mx-auto py-28 px-10">
       <div className="grid grid-cols-12 gap-5">
         <div className="col-span-4">
           <h1 className="font-bold text-7xl text-zinc-900">Better To Think For Learning</h1>
         </div>
         <div className="col-span-8 flex relative justify-end">
-          <div className="w-92 bg-zinc-600 h-124 mt-18 rounded-bl-[14rem] overflow-hidden shadow-[-7px_7px_0_rgba(0,0,0,0.2)]">
+          <div className="w-92 bg-zinc-600 h-124 mt-18 rounded-bl-[14rem] overflow-hidden shadow-[-7px_7px_0_rgba(0,0,0,0.2)] z-10">
             <video
               src={vid1}
               className="w-full h-full object-cover" autoPlay loop muted playsInline
@@ -21,7 +21,9 @@ const Hero = () => {
               src={vid2}
               className="w-full h-full object-cover" autoPlay loop muted playsInline></video>
           </div>
+          <div className="h-56 w-56 aspect-square bg-[#4B58B8] absolute right-[40%] z-0 -top-8 rounded-full"></div>
           <img src={star} alt="" className=' animate-slowbounce absolute right-20 z-20' />
+          <div className="w-38 h-38 aspect-square rounded-full bg-[#E44D30] absolute -bottom-16 animate-slowbounce left-20 z-20"></div>
           <div className="absolute w-26 h-60 bg-[#E9BD38] bottom-0 rounded-t-full rounded-b-full z-0 -right-10"></div>
         </div>
       </div>
