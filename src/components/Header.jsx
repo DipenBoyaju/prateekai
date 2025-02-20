9e8dfb4951c41458bc34e8b91afa7ebfeab464c7
Dipen <dipendraboyaju222@gmail.com>
2025-02-20 21:52:06 +0545

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import Navbar from "./Navbar"
 const Header = () => {
   return (
     <div className=" border-zinc-300">
-      <div className="flex items-center justify-between container mx-auto py-4 px-10">
+      <div className="flex items-center justify-between container mx-auto py-4 px-4">
         <div className="">
           <h1 className="text-2xl font-semibold uppercase">Prateek</h1>
         </div>
         <Navbar />
-        <button className="text-lg font-semibold font-mono border border-gold py-2 px-4 rounded-sm bg-primary text-white">Contribute</button>
+        <button className="text-sm md:text-lg md:font-semibold font-mono border border-gold py-2 px-4 rounded-sm bg-primary text-white">Contribute</button>
       </div>
     </div>
   )
