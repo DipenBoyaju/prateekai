cb4515546e84a9512684daa48779fbcd887c4bed
Dipen <dipendraboyaju222@gmail.com>
2025-02-27 13:27:32 +0545

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom"
 import Navbar from "./Navbar"
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const nav = useNavigate();
   return (
     <div className=" border-b border-zinc-300  w-full z-50 shadow-xs ">
       <div className="flex items-center justify-between container mx-auto py-4 px-4 md:px-10">
-        <div className="">
+        <div className="" >
           {/* <h1 className="text-2xl font-semibold uppercase">Prateek</h1> */}
-          <img src="/images/logotext.png" alt="" className="w-50" />
+          <img src="/images/logotext.png" alt="" className="w-50 cursor-pointer" onClick={() => nav('/')} />
         </div>
         <Navbar />
         <Link to='/contribute'>
