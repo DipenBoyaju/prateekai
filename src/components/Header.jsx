58cb0e4d06a2e24e68003af76765434ca3b37e24
Dipen <dipendraboyaju222@gmail.com>
2025-03-02 22:01:16 +0545

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,27 +12,27 @@ const Header = ({ name }) => {
           {/* <img src="/images/logotext.png" alt="" className="w-30 md:w-50 cursor-pointer" onClick={() => nav('/')} /> */}
           {name === "mon" && <div className="flex gap-2 items-center">
             <img src="/images/prateek.png" className="size-9" alt="" />
-            <h2 className="font-montserrat font-semibold text-2xl text-zinc-800"> Prateek</h2>
-            <p className="text-xs text-zinc-500">(Montserrat)</p>
+            <h2 className="font-montserrat font-semibold text-2xl text-blue"> Prateek</h2>
+            {/* <p className="text-xs text-zinc-500">(Montserrat)</p> */}
           </div>}
           {/* <div className="flex gap-2 items-center">
             <img src="/images/prateek.png" className="size-9" alt="" />
-            <h2 className="font-nunito font-bold text-2xl text-zinc-800"> Prateek</h2>
+            <h2 className="font-nunito font-bold text-2xl text-blue"> Prateek</h2>
             <p className="text-xs text-zinc-500">(nunito)</p>
           </div> */}
           {name === "quick" && <div className="flex gap-2 items-center">
             <img src="/images/prateek.png" className="size-9" alt="" />
-            <h2 className="font-quicksand font-bold text-2xl text-zinc-800"> Prateek</h2>
-            <p className="text-xs text-zinc-500">(quicksand)</p>
+            <h2 className="font-quicksand font-bold text-2xl text-blue"> Prateek</h2>
+            {/* <p className="text-xs text-zinc-500">(quicksand)</p> */}
           </div>}
           {name === "pop" && <div className="flex gap-2 items-center">
             <img src="/images/prateek.png" className="size-9" alt="" />
-            <h2 className="font-poppins font-semibold text-2xl text-zinc-800"> Prateek</h2>
-            <p className="text-xs text-zinc-500">(poppins)</p>
+            <h2 className="font-poppins font-semibold text-2xl text-blue"> Prateek</h2>
+            {/* <p className="text-xs text-zinc-500">(poppins)</p> */}
           </div>}
           {/* <div className="flex gap-2 items-center">
             <img src="/images/prateek.png" className="size-9" alt="" />
-            <h2 className="font-lexend font-semibold text-2xl text-zinc-800"> Prateek</h2>
+            <h2 className="font-lexend font-semibold text-2xl text-blue"> Prateek</h2>
             <p className="text-xs text-zinc-500">(Lexend Deca)</p>
           </div> */}
           {/* <div className="flex gap-2 items-center">
